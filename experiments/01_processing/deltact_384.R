cc67d902a3c2ec1f9695e22606a9c1bd9d584b69
IrisNM <mollhoff@stanford.edu>
2024-01-22 22:04:03 -0800

    Still messing with stuff
    hkg_cq collumn isn't showing filled in

--- experiments/01_processing/deltact_384.R
+++ experiments/01_processing/deltact_384.R
@@ -1,5 +1,5 @@
 #function to combine tables and calculate delta-Ct
-deltact_384 <- function(dataframe,housekeeping_df, marker_gene,list_for_join){  
+deltact_384 <- function(dataframe,housekeeping_df, marker_gene){  
   #dataframe= a data frame, df1 from calctecrep.R
   #housekeeping_df=a data frame, df_hkg from pullhkg.R
   #marker_gene=a string, name of marker as appears in df1 primers column
@@ -10,12 +10,13 @@ deltact_384 <- function(dataframe,housekeeping_df, marker_gene,list_for_join){
     select(!cq_sd_tecrep,marker_cq=cq_avg_tecrep)
   
   df_merge <- df_marker %>% #merge marker gene dataframe with housekeeping gene dataframe
-    left_join(y=housekeeping_df, by=list_for_join) %>%  #example: by=c("Sample","genotype","treatment")
-    relocate(Target.x, .before = marker_cq) %>% 
+    left_join(y=housekeeping_df) %>%  #example: by=c("Sample","genotype","treatment")
     relocate(Sample)
  
-  df_dct <- df_merge %>% 
-    mutate(d_cq=marker_cq-hkg_cq)
+  return(df_merge)
   
-  return(list(df_dct, df_marker))
+  #df_dct <- df_merge %>% 
+  #  mutate(d_cq=marker_cq-hkg_cq)
+  
+  #return(list(df_dct, df_marker)) #returns list of tables (1)is delta cq or delta ct. (2) is the isolated marker gene
 }
