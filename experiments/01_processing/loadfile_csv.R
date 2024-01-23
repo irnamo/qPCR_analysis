559c5c12cfe79673000a1596c54c56a1b485a1de
IrisNM <mollhoff@stanford.edu>
2024-01-22 20:32:48 -0800

    Change Loadfile script
    made new loadfile_csv script and modified rmd with datafolder variable

---
+++ experiments/01_processing/loadfile_csv.R
@@ -0,0 +1,11 @@
+#function to load data files
+#by Iris N. Mollhoff
+##load packages
+library(tidyverse)
+library(readxl)
+##load data files
+loadfile_csv <- function(folder, file_csv){
+  data_dir <- paste(folder,file_csv,".csv", sep = "")
+  table_csv <- read_csv(data_dir)
+  return(table_csv)
+}
